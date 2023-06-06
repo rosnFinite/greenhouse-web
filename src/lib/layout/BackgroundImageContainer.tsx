2f26a52a12bdc02681c9d875dd2bdbc3575e3b91
Fabio Rose <rosef@hochschule-trier.de>
2023-06-06 03:09:25 +0200

    Hintegrund Margin behoben

--- src/lib/layout/BackgroundImageContainer.tsx
+++ src/lib/layout/BackgroundImageContainer.tsx
@@ -1,13 +1,7 @@
-import { Container } from "@nextui-org/react";
-
 export default function BackgroundImageContainer({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <Container fluid className="background" gap={0}>
-      {children}
-    </Container>
-  );
+  return <div className="background">{children}</div>;
 }
