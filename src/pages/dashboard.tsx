a5298a6692e7218d7adeb53db863668024faa0d6
Fabio Rose <rosef@hochschule-trier.de>
2023-06-23 18:19:34 +0200

    Mock Layout für Dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,4 +1,4 @@
-import { Container, Text } from "@nextui-org/react";
+import { Container, Text, Grid, Card } from "@nextui-org/react";
 import dynamic from "next/dynamic";
 import type { ReactElement } from "react";
 
@@ -6,10 +6,61 @@ import type { NextPageWithLayout } from "./_app";
 
 const NavbarContainer = dynamic(() => import("lib/layout/NavbarContainer"));
 
+const MockItem = ({ text }: { text: string }): JSX.Element => {
+  return (
+    <Card css={{ $$cardColor: "$colors$success" }}>
+      <Card.Body>
+        <Text h6 size={15} color="white" css={{ m: 0 }}>
+          {text}
+        </Text>
+        <Text size={15}>sadaksd</Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
 const Dashboard: NextPageWithLayout = () => {
   return (
-    <Container display="flex" css={{ height: "100vh" }}>
-      <Text>Dashboard</Text>
+    <Container
+      display="flex"
+      alignContent="flex-start"
+      css={{ height: "100vh" }}
+    >
+      <Grid.Container
+        gap={2}
+        justify="center"
+        css={{
+          padding: 0,
+          height: "200px",
+          marginBottom: "$10",
+          marginTop: "$10",
+        }}
+      >
+        <Grid xs={12} md={4}>
+          <MockItem text="Temperatur" />
+        </Grid>
+        <Grid xs={12} md={4}>
+          <MockItem text="Luftfeuchtigkeit" />
+        </Grid>
+        <Grid xs={12} md={4}>
+          <MockItem text="Pflanzengröße" />
+        </Grid>
+      </Grid.Container>
+      <Grid.Container
+        gap={2}
+        justify="center"
+        css={{ padding: 0, height: "200px" }}
+      >
+        <Grid xs={12} md={4}>
+          <MockItem text="Bodenfeuchtigkeit 1" />
+        </Grid>
+        <Grid xs={12} md={4}>
+          <MockItem text="Bodenfeuchtigkeit 2" />
+        </Grid>
+        <Grid xs={12} md={4}>
+          <MockItem text="Bodenfeuchtigkeit 3" />
+        </Grid>
+      </Grid.Container>
     </Container>
   );
 };
