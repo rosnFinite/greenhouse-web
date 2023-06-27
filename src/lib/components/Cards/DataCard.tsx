9a6850a429af3a59bd186a0b4bbb4cf6c0338bc3
Fabio Rose <rosef@hochschule-trier.de>
2023-06-27 18:55:25 +0200

    chart v1

---
+++ src/lib/components/Cards/DataCard.tsx
@@ -0,0 +1,36 @@
+import { Card, Grid, Text } from "@nextui-org/react";
+import { Line, LineChart, ResponsiveContainer, Tooltip } from "recharts";
+
+interface DataCardProps {
+  titleName: string;
+  data: { timestamp: string; data: number }[];
+}
+
+export default function DataCard({ titleName, data }: DataCardProps) {
+  return (
+    <Card variant="shadow" css={{ $$cardColor: "$color$success" }}>
+      <Card.Header>
+        <Text>{titleName}</Text>
+      </Card.Header>
+      <Card.Body>
+        <Grid.Container gap={2}>
+          <Grid xs={8}>
+            <ResponsiveContainer width="100%" height="100%">
+              <LineChart data={data}>
+                <Line
+                  type="monotone"
+                  dataKey="data"
+                  stroke="#FFAFAF"
+                  strokeWidth={5}
+                />
+              </LineChart>
+            </ResponsiveContainer>
+          </Grid>
+          <Grid xs={4}>
+            <Text size={45}>{data.slice(-1)[0].data}</Text>
+          </Grid>
+        </Grid.Container>
+      </Card.Body>
+    </Card>
+  );
+}
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -2,6 +2,8 @@ import { Container, Text, Grid, Card } from "@nextui-org/react";
 import dynamic from "next/dynamic";
 import type { ReactElement } from "react";
 
+import DataCard from "lib/components/Cards/DataCard";
+
 import type { NextPageWithLayout } from "./_app";
 
 const NavbarContainer = dynamic(() => import("lib/layout/NavbarContainer"));
@@ -18,6 +20,117 @@ const MockItem = ({ text }: { text: string }): JSX.Element => {
     </Card>
   );
 };
+
+const mockData = [
+  {
+    timestamp: "Page A",
+    temperature: 22.4,
+    humidity: 77.3,
+    plantSize: 62.6,
+    soilhumidity1: 78,
+    soilhumidity2: 72,
+    soilhumidity3: 77,
+  },
+  {
+    timestamp: "Page B",
+    temperature: 22.4,
+    humidity: 77.8,
+    plantSize: 62.6,
+    soilhumidity1: 77,
+    soilhumidity2: 71.4,
+    soilhumidity3: 75.2,
+  },
+  {
+    timestamp: "Page C",
+    temperature: 23.5,
+    humidity: 79,
+    plantSize: 62.6,
+    soilhumidity1: 76.4,
+    soilhumidity2: 71,
+    soilhumidity3: 74.3,
+  },
+  {
+    timestamp: "Page D",
+    temperature: 23.1,
+    humidity: 83.4,
+    plantSize: 62.6,
+    soilhumidity1: 75.4,
+    soilhumidity2: 70.2,
+    soilhumidity3: 73.8,
+  },
+  {
+    timestamp: "Page E",
+    temperature: 23.8,
+    humidity: 81.4,
+    plantSize: 62.6,
+    soilhumidity1: 74.1,
+    soilhumidity2: 69.6,
+    soilhumidity3: 72.6,
+  },
+  {
+    timestamp: "Page F",
+    temperature: 25,
+    humidity: 77.2,
+    plantSize: 62.6,
+    soilhumidity1: 73.3,
+    soilhumidity2: 69.1,
+    soilhumidity3: 72.1,
+  },
+  {
+    timestamp: "Page G",
+    temperature: 25.6,
+    humidity: 73.9,
+    plantSize: 62.6,
+    soilhumidity1: 71.4,
+    soilhumidity2: 67.9,
+    soilhumidity3: 70.3,
+  },
+  {
+    timestamp: "Page G",
+    temperature: 25.3,
+    humidity: 78.3,
+    plantSize: 62.6,
+    soilhumidity1: 70.2,
+    soilhumidity2: 66.9,
+    soilhumidity3: 68.2,
+  },
+  {
+    timestamp: "Page G",
+    temperature: 25,
+    humidity: 77.2,
+    plantSize: 62.6,
+    soilhumidity1: 84.9,
+    soilhumidity2: 88.2,
+    soilhumidity3: 83.8,
+  },
+  {
+    timestamp: "Page G",
+    temperature: 25.6,
+    humidity: 77.8,
+    plantSize: 62.9,
+    soilhumidity1: 84,
+    soilhumidity2: 87.5,
+    soilhumidity3: 83,
+  },
+  {
+    timestamp: "Page G",
+    temperature: 24.7,
+    humidity: 77.6,
+    plantSize: 62.9,
+    soilhumidity1: 83.4,
+    soilhumidity2: 86.8,
+    soilhumidity3: 82.1,
+  },
+  {
+    timestamp: "Page G",
+    temperature: 24.9,
+    humidity: 78.6,
+    plantSize: 62.9,
+    soilhumidity1: 82.9,
+    soilhumidity2: 86.1,
+    soilhumidity3: 81.7,
+  },
+];
 
 const Dashboard: NextPageWithLayout = () => {
   return (
@@ -37,7 +150,13 @@ const Dashboard: NextPageWithLayout = () => {
         }}
       >
         <Grid xs={12} md={4}>
-          <MockItem text="Temperatur" />
+          <DataCard
+            titleName="Temperatur"
+            data={mockData.map(({ timestamp, temperature: data }) => ({
+              timestamp,
+              data,
+            }))}
+          />
         </Grid>
         <Grid xs={12} md={4}>
           <MockItem text="Luftfeuchtigkeit" />
