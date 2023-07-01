56078d6337b49be99bd0a984d48971c25301930f
Fabio Rose <rosef@hochschule-trier.de>
2023-07-01 18:59:30 +0200

    /devices + DevicesCard für Gerätinformationen

--- src/pages/devices.tsx
+++ src/pages/devices.tsx
@@ -1,15 +1,55 @@
-import { Container, Text } from "@nextui-org/react";
+import { Container, Text, Grid } from "@nextui-org/react";
 import dynamic from "next/dynamic";
 import type { ReactElement } from "react";
 
+import DeviceCard from "lib/components/Cards/DeviceCard";
+
 import type { NextPageWithLayout } from "./_app";
 
+const AddDevicesModal = dynamic(
+  () => import("lib/components/Buttons/AddDevicesModal")
+);
 const NavbarContainer = dynamic(() => import("lib/layout/NavbarContainer"));
 
 const Devices: NextPageWithLayout = () => {
   return (
-    <Container display="flex" css={{ height: "100vh" }}>
-      <Text>Devices</Text>
+    <Container css={{ height: "100vh" }}>
+      <Text h2 css={{ marginTop: "$10" }}>
+        Meine Geräte (2)
+      </Text>
+      <Grid.Container
+        gap={2}
+        justify="center"
+        css={{ padding: 0, marginBottom: "$10", marginTop: "$10" }}
+      >
+        <Grid xs={12}>
+          <DeviceCard
+            deviceName="Mein Gewächshaus"
+            modelName="GRW-231-XYZ"
+            imgUrl="/growbox.webp"
+            isOnline
+            sizeText="110cm x 200cm x 110cm"
+            areaText="1,21cm²"
+            hasTempSensor
+            hasHumSensor
+            numSoilSensors={3}
+          />
+        </Grid>
+        <Grid xs={12}>
+          <DeviceCard
+            deviceName="Das Monster"
+            modelName="GRW-231-XYZ"
+            imgUrl="/growbox.webp"
+            isOnline={false}
+            sizeText="110cm x 200cm x 110cm"
+            areaText="1,21cm²"
+            hasTempSensor
+          />
+        </Grid>
+        <Grid xs={12}>
+          <AddDevicesModal />
+        </Grid>
+      </Grid.Container>
     </Container>
   );
 };
